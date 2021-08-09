d4e1cc9104391abf1c3f9f54bbec1bf8e4a31679
root <daintree.henry@gmail.com>
2021-08-10 01:01:01 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+﻿var express = require('express');
+var request = require('request');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    let data = new Object()
+    const itemurl = "http://"+process.env.ItemURL+"/api/items"
+    const empurl = "http://"+process.env.EmpURL+"/api/employees"
+    console.log(itemurl)
+    
+    request(itemurl, function (error, response, body) {
+        if (!error && response.statusCode < 300) {
+            data.items = JSON.parse(body)
+        }else{
+            data.items = "Server Error"
+        }
+        request(empurl, function (error, response, body) {
+            if (!error && response.statusCode < 300) {
+                data.emps = JSON.parse(body)
+                res.render('index', {data})
+            }else{
+                data.emps = "Server Error"
+                res.render('index', {data})
+            }
+        });
+    });
+});
+
+module.exports = router;
