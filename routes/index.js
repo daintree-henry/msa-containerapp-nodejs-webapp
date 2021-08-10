273d3bcc4fd398e59de6b4efdc8221822a1c6205
henry hyeonwoo hwang <73600675+daintree-henry@users.noreply.github.com>
2021-08-11 04:13:42 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,13 @@ router.get('/', function (req, res) {
     const empurl = "http://"+process.env.EmpURL+"/api/employees"
     console.log(itemurl)
     
-    request(itemurl, function (error, response, body) {
+    request({uri: itemurl, method: "GET", timeout: 5000}, function (error, response, body) {
         if (!error && response.statusCode < 300) {
             data.items = JSON.parse(body)
         }else{
             data.items = "Server Error"
         }
-        request(empurl, function (error, response, body) {
+        request({uri: empurl, method: "GET", timeout: 5000}, function (error, response, body) {
             if (!error && response.statusCode < 300) {
                 data.emps = JSON.parse(body)
                 res.render('index', {data})
