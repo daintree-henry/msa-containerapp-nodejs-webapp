cd1d6e7a06a89ea85e745f60f8fbf9ec1c0b130e
daintree-henry <daintree.henry@gmail.com>
2021-12-14 10:01:56 +0900

    update yaml

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/webapp',express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
